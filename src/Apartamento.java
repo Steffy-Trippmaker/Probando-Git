680a9f4516f30d0db327923565b026948bf3c160
Steffy Trippmaker <steffytrippmaker@gmail.com>
2022-07-29 13:35:54 -0300

    Clase apartamento

--- src/Apartamento.java
+++ src/Apartamento.java
@@ -1,4 +1,14 @@
 
-public class Apartamento {
+public class Apartamento extends Inmueble {
+
+	private byte cantidadPisos;
+	private byte cantAptoPiso;
+
+	public Apartamento(String ID, String nombre, String direccion, byte catidadPisos, byte cantAptoPiso) {
+		super(ID, nombre, direccion);
+		this.cantidadPisos = cantidadPisos;
+		this.cantAptoPiso = cantAptoPiso;
+
+	}
 
 }
