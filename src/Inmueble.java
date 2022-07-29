a835910f432dc23c5b62f85f1cf2f6aeb6a77092
Steffy Trippmaker <steffytrippmaker@gmail.com>
2022-07-29 12:03:09 -0300

    Creando la primera clase Inmueble

---
+++ src/Inmueble.java
@@ -0,0 +1,13 @@
+
+public class Inmueble {
+
+	private String ID;
+	private String nombre;
+	private String direccion;
+
+	public Inmueble(String ID, String nombre, String direccion) {
+		this.ID = ID;
+		this.nombre = nombre;
+		this.direccion = direccion;
+	}
+}
